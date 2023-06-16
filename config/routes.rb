e317e0309a4aaac9d2d506dd9e8a3a5b5d407281
Henchworm <agop5134@gmail.com>
2023-06-16 11:12:33 -0600

    fixes routes for m3

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-
-  resources :wiki_posts, path: 'wiki_posts'
-
-  namespace :welcome do
-    get 'about', to: 'about', as: 'about'
-  end
-
+  resources :wiki_posts
+  namespace :wiki_posts do 
+    get 'example'
+  end 
+  namespace :welcome do 
+    get 'index'
+    get 'about'
+    get 'archive'
+  end 
+ 
   get '/about', to: redirect('/welcome/about')
-
+  root "welcome#index"
 end
